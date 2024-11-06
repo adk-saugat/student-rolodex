9cf3a20eb17e582fb2bb21c4b4df6d805edd35b6
adk-saugat <adhikarisaugat34@gmail.com>
2024-11-05 18:39:35 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      students: [
+        {
+          id: '101',
+          name: "Saugat",
+        },
+        {
+          id: '102',
+          name: "Samarpan",
+        },
+        {
+          id: '103',
+          name: "Suman",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.students.map((student) => {
+          return <h1 key={student.id}>{student.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
