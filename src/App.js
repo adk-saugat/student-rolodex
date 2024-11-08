6d2f20223bbc5599c7de1587a1a70a5c11fcc6cc
adk-saugat <adhikarisaugat34@gmail.com>
2024-11-08 12:24:46 -0600

    changing class to functional components

--- src/App.js
+++ src/App.js
@@ -1,53 +1,86 @@
-import { Component } from "react";
-import CardList from "./components/card-list/CardList";
-import SearchField from "./components/search-field/SearchField";
-import "./App.css";
+import CardList from "./components/card-list/CardList"
+import SearchField from "./components/search-field/SearchField"
+import "./App.css"
+import { useState, useEffect } from "react"
 
-class App extends Component {
-  constructor() {
-    super();
+function App() {
+  const [searchValue, setSearchValue] = useState("")
+  const [students, setStudents] = useState([])
+  const [filteredStudents, setFilteredStudents] = useState(students)
 
-    this.state = {
-      students: [],
-      searchField: "",
-    };
+  const handleSearch = (event) => {
+    const searchField = event.target.value.toLowerCase()
+    setSearchValue(searchField)
   }
+  useEffect(() => {
+    const newfilteredStudents = students.filter((student) => {
+      return student.name.toLowerCase().includes(searchValue)
+    })
+    setFilteredStudents(newfilteredStudents)
+  }, [students, searchValue])
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((student) =>
-        this.setState(() => {
-          return { students: student };
-        })
-      );
-  }
+      .then((student) => setStudents(student))
+  }, [])
 
-  handleSearch = (event) => {
-    const searchField = event.target.value.toLowerCase();
-    this.setState(() => {
-      return { searchField };
-    });
-  };
-
-  render() {
-    const { students, searchField } = this.state;
-    const { handleSearch } = this;
-    const filteredStudents = students.filter((student) => {
-      return student.name.toLowerCase().includes(searchField);
-    });
-    return (
-      <div className="App">
-        <h1 className="app-title">Student Rolodex</h1>
-        <SearchField
-          onSearch={handleSearch}
-          placeholder="Search Students"
-          className="student-search-box"
-        />
-        <CardList students={filteredStudents} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1 className="app-title">Student Rolodex</h1>
+      <SearchField
+        onSearch={handleSearch}
+        placeholder="Search Students"
+        className="student-search-box"
+      />
+      <CardList students={filteredStudents} />
+    </div>
+  )
 }
+export default App
+// class App extends Component {
+//   constructor() {
+//     super();
 
-export default App;
+//     this.state = {
+//       students: [],
+//       searchField: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((student) =>
+//         this.setState(() => {
+//           return { students: student };
+//         })
+//       );
+//   }
+
+//   handleSearch = (event) => {
+//     const searchField = event.target.value.toLowerCase();
+//     this.setState(() => {
+//       return { searchField };
+//     });
+//   };
+
+//   render() {
+//     const { students, searchField } = this.state;
+//     const { handleSearch } = this;
+//     const filteredStudents = students.filter((student) => {
+//       return student.name.toLowerCase().includes(searchField);
+//     });
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">Student Rolodex</h1>
+//         <SearchField
+//           onSearch={handleSearch}
+//           placeholder="Search Students"
+//           className="student-search-box"
+//         />
+//         <CardList students={filteredStudents} />
+//       </div>
+//     );
+//   }
+// }
